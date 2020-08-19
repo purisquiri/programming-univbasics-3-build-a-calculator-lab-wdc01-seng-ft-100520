8ecc2209564cb5147553fa301d14794c98f08447
Mauro Dorigo <mau.dorigo@outlook.es>
2020-08-19 21:43:32 +0000

    Automatically backed up by Learn

--- lib/math.rb
+++ lib/math.rb
@@ -4,26 +4,26 @@ def addition(num1, num2)
 end
 
 def subtraction(num1, num2)
-  num1 - num2
-  subtraction
+  subs = num1 - num2
+  subs
 end
 
 def multiplication(num1, num2)
-  num1 * num2
-  multiplication
+  product = num1 * num2
+  product
 end
 
 def division(num1, num2)
-  num1 / num2
-  division
+  div = num1 / num2
+  div
 end
 
 def modulo(num1, num2)
-  num1 % num2
-  modulo
+  mod = num1 % num2
+  mod
 end
 
 def square_root(num)
-  num ** num
-  square_root
+  squre = num ** num
+  square
 end
